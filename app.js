40f81e79fce98268b5fecb7ab16d571b3206a0f1
dallas-vanwyk <dallas.vanwyk@gmail.com>
2024-12-05 17:25:49 -0600

    completed exercise 3, updated exercise 1, tried to update exercise 2 but was not successful

--- app.js
+++ app.js
@@ -108,9 +108,7 @@ let veryOldInventors = [];
 // Complete the exercise in the space below:
 
 veryOldInventors = inventors.filter((inventor) => {
-    if (inventor.year >= 1500 && inventor.year <= 1599) {
-        return inventor
-    }
+    return (inventor.year >= 1500 && inventor.year <= 1599)
 });
 
 
@@ -144,6 +142,9 @@ let inventorNames = [];
 
 inventorNames = inventors.map((inventor) => {
     return {first: inventor.first, last: inventor.last};
+    // delete inventor.year; // this didn't work because it modified the original 'inventors' array ¯\_(ツ)_/¯
+    // delete inventor.passed;
+    // return inventor;
 });
 
 // Check your work:
@@ -163,4 +164,39 @@ console.log('Exercise 2 correct result: ', [
     { first: 'Hanna', last: 'Hammarström' },
 ]);
 
+
+/*
+Exercise 3: Array.prototype.sort()
+
+Sort the inventors by birth date in ascending order (from those born furthest in
+the past to those born most recently).
+*/
+
+let sortedByBirthYear = [];
+
+// Complete the exercise in the space below:
+
+// console.log("just checking...");
+// console.log(inventors);
+
+sortedByBirthYear = inventors.toSorted((a,b) => a.year - b.year);
+
+// thanks, MDN!
+
+// Check your work:
+console.log('Exercise 3 my result: ', sortedByBirthYear);
+console.log('Exercise 3 correct result: ', [
+  { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+  { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+  { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+  { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+  { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+  { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 },
+  { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+  { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+  { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+  { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+  { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+  { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+]);
 
